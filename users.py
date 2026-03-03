d62f46f248cab6e1d71ed318d759392360baef2d
Anthony <anthonydeepak.ch1@gmail.com>
2026-03-04 00:11:06 +0530

    DB configured for users.py

--- users.py
+++ users.py
@@ -1,5 +1,5 @@
 from ast import Add
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from pydantic import BaseModel, Field , field_validator
 from typing import Optional
@@ -7,6 +7,10 @@ from typing import Optional
 from datetime import datetime
 
 
+from db_conf import get_db
+import db_models
+
+from sqlalchemy.orm import Session
 
 
 #routers for user-related endpoints
@@ -14,6 +18,7 @@ from datetime import datetime
 router = APIRouter()
 
 class User(BaseModel):
+    id : int = Field(default_factory=lambda: len(users)+1, description="ID for the user")
     name: str
     age: int
     gender: str= Field(description="Gender of the user")
@@ -34,10 +39,26 @@ class Users(BaseModel):
 users = []
 
 @router.get("/users", response_model=Users)
-def get_user():
+def get_user(db: Session = Depends(get_db)):
+    users = db.query(db_models.User).all()
+    #users = [User(id=user.id, name=user.name, age=user.age, gender=user.gender, Address=user.Address) for user in db_users]
     return Users(data=users, message="success")
 
 @router.post("/users")
-def create_user(user: User):
-    users.append(user.model_dump())
-    return {"data": user, "message": "user created successfully"}
+def create_user(user: User , db: Session = Depends(get_db)):
+    # users.append(user.model_dump())
+    # return {"data": user, "message": "user created successfully"}
+
+    # users = db_models.User(
+    #     name=user.name,
+    #     age=user.age,
+    #     gender=user.gender,
+    #     Address=user.Address
+    # )
+    users = db_models.User(**user.model_dump()) # As model_dump() returns a dictionary of the model's data, we can unpack it using ** to create an instance of db_models.User with the same attributes as the User model.
+    db.add(users)
+    db.commit()
+    db.refresh(users)
+    return {"data": users, "message": "user created successfully"}
+
+
