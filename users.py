b8857668aeb2c06992c6b6582f8f4582efb0bde9
Anthony <anthonydeepak.ch1@gmail.com>
2026-03-04 17:19:06 +0530

    get-post to db for users added

--- users.py
+++ users.py
@@ -29,6 +29,10 @@ class User(BaseModel):
         if value < 0:
             raise ValueError("Age must be a positive integer")
         return value
+    
+    model_config = {                # It tells Pydantic: If input is an object, read its attributes instead of expecting dict.
+        "from_attributes": True     # This configuration allows Pydantic to create a User model instance from an instance of the db_models.User class, which is useful when retrieving user data from the database and returning it in the API response.
+    }
 
 class Users(BaseModel):
     data: list[User]
@@ -40,9 +44,11 @@ users = []
 
 @router.get("/users", response_model=Users)
 def get_user(db: Session = Depends(get_db)):
-    users = db.query(db_models.User).all()
-    #users = [User(id=user.id, name=user.name, age=user.age, gender=user.gender, Address=user.Address) for user in db_users]
-    return Users(data=users, message="success")
+    users = db.query(db_models.User).order_by(db_models.User.id).all()  # This line retrieves all user records from the database, orders them by their ID, and returns the result as a list of db_models.User instances. The .all() method is called twice to first execute the query and then to retrieve the results as a list.
+    #print(users)
+    count_of_users = db.query(db_models.User).count()
+    #return Users(data=users, message="success")  # Added model_config in User pydantic class. Error :  Input should be a valid dictionary or instance of User [type=model_type, input_value=<db_models.User object at 0x0000011428169D90>, input_type=User] For further information visit https://errors.pydantic.dev/2.12/v/model_type
+    return {"data": users, "message": f"success retrieved {count_of_users} users"}
 
 @router.post("/users")
 def create_user(user: User , db: Session = Depends(get_db)):
